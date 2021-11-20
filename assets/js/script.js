a60532623002fde9aed85feb8a692347b87c6a8f
Jocelyn Tian <jocelyntianww@gmail.com>
2021-11-20 14:56:32 -0700

    add html outline

---
+++ assets/js/script.js
@@ -0,0 +1,65 @@
+const questions = [
+  {
+    question: "What is the command we use to create a new file? ",
+    answers: {
+      a: "touch",
+      b: "pwd",
+      c: "mkdir",
+      d: "cd",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question:
+      "When creating a form with HTML, which of the following is not typically used? ",
+    answers: {
+      a: "<label> for the name of the input field",
+      b: "<input> for the type of input the user is expected to provide, like text or email",
+      c: "<header> for the title of the form",
+      d: "<button> for allowing the user to submit their data after they have filled out the form",
+    },
+    correctAnswer: "c",
+  },
+  {
+    question:
+      "When using flexbox, which property needs to be adjusted in order to add space between items? ",
+    answers: {
+      a: "justify-content",
+      b: "flex-flow",
+      c: "align-content",
+      d: "space-between",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question: "What is one advantage of Responsive Design for a developer? ",
+    answers: {
+      a: "Faster page loading time",
+      b: "Faster development",
+      c: "More social sharing",
+      d: "Improved SEO",
+    },
+    correctAnswer: "b",
+  },
+  {
+    question:
+      "Which of the following type of variable is visible everywhere in your JavaScript code?",
+    answers: {
+      a: "global variable",
+      b: "local variable",
+      c: "Both of the above.",
+      d: "None of the above.",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    answers: {
+      a: "<javascript",
+      b: "scripting",
+      c: "script",
+      d: "js",
+    },
+    correctAnswer: "c",
+  },
+];
